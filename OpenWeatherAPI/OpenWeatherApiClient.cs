87af8014cca415b2d8b33ff6ea4a147fcbca03a6
Skratymir <97696626+Skratymir@users.noreply.github.com>
2023-02-09 23:12:38 +0000

    Integrate changes from master

--- OpenWeatherAPI/OpenWeatherApiClient.cs
+++ OpenWeatherAPI/OpenWeatherApiClient.cs
@@ -16,6 +16,21 @@ namespace OpenWeatherAPI
 			_useHttps = useHttps;
 		}
 
+		private async Task<Uri> GenerateRequestUrl(string queryString)
+		{
+			var geo = await Geolocate(queryString).ConfigureAwait(false);
+
+			return new Uri($"http://api.openweathermap.org/data/2.5/weather?appid={_apiKey}&lat={geo.Lat}&lon={geo.Lon}");
+		}
+
+		public async Task<GeoResponse> Geolocate(string queryString)
+		{
+			var jsonResponse = await _httpClient
+				.GetStringAsync(
+					new Uri($"http://api.openweathermap.org/geo/1.0/direct?q={queryString}&limit={1}&appid={_apiKey}"))
+				.ConfigureAwait(false);
+			return new GeoResponse(jsonResponse);
+		}
 		private Uri GenerateRequestUrl(string queryString)
 		{
 			string scheme = "http";
@@ -32,7 +47,7 @@ namespace OpenWeatherAPI
 		/// <returns>Returns null if the query is invalid.</returns>
 		public async Task<QueryResponse> QueryAsync(string queryString)
 		{
-			var jsonResponse = await _httpClient.GetStringAsync(GenerateRequestUrl(queryString)).ConfigureAwait(false);
+			var jsonResponse = await _httpClient.GetStringAsync(GenerateRequestUrl(queryString).Result).ConfigureAwait(false);
 			var query = new QueryResponse(jsonResponse);
 			return query.ValidRequest ? query : null;
 		}
